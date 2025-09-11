def89483298f59a25ea42669d637396588e0a1b8
phambao159 <pgiabao159@gmail.com>
2025-09-11 12:53:48 +0700

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import { Route, Routes } from "react-router-dom";
 import AppPetOwner from "../src/Pages/PetOwner/AppPetOwner";
+import Calendar from "./Pages/calendar";
 
 
 
@@ -6,6 +8,9 @@ export default function App() {
   return (
     <div className="test">
       <AppPetOwner />
+      <Routes>
+        <Route path="/calendar" element={<Calendar />} ></Route>
+      </Routes>
     </div>
   );
 }
