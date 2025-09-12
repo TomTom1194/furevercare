7a3b90d6c00874c9dbb685a75ffccef11da5decc
TomTom1194 <tangthientan10194@gmail.com>
2025-09-12 11:07:36 +0700

    Revert "Feature/vets2"

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+
+
 import { Route, Routes } from "react-router-dom";
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
@@ -10,16 +12,17 @@ import VetLayout from "./layouts/VetLayout";
 //Start
 import Home from "./components/Home/Home";
 
+
 //Petowner
-import HomePetowner from "./pages/PetOwner/home";
-import PetCare from "./pages/PetOwner/petcare";
-import PetProduct from "./pages/PetOwner/petproduct";
-import FeedBack from "./pages/PetOwner/feedback";
-import AboutUs from "./pages/PetOwner/aboutus";
-import ContactUs from "./pages/PetOwner/contactus";
-import EmerPage from "./pages/PetOwner/emerpage";
-import PetDetail from "./pages/PetOwner/petdetail";
-import ProductDetail from "./pages/PetOwner/productdetail";
+import HomePetowner from './pages/PetOwner/home';
+import PetCare from './pages/PetOwner/petcare';
+import PetProduct from './pages/PetOwner/petproduct';
+import FeedBack from './pages/PetOwner/feedback';
+import AboutUs from './pages/PetOwner/aboutus';
+import ContactUs from './pages/PetOwner/contactus';
+import EmerPage from './pages/PetOwner/emerpage';
+import PetDetail from './pages/PetOwner/petdetail';
+import ProductDetail from './pages/PetOwner/productdetail';
 import petcare from "../src/Data/Petowner/petcare.json";
 import petproduct from "../src/Data/Petowner/petproduct.json";
 
@@ -33,7 +36,7 @@ import Event from "./Component/animalshelter/event/Event";
 import ShelterContact from "./Component/animalshelter/sheltercontact/ShelterContact";
 
 //Vet
-import HomeVet from "./pages/Veterinarian/HomeVeterinarian";
+import HomeVet from "./pages/Veterinarian/HomeVet";
 import VetProfile from "./pages/Veterinarian/VetProfile";
 
 export default function App() {
@@ -46,26 +49,20 @@ export default function App() {
 
         {/* PetOwner routes */}
         <Route path="/petowner" element={<PetOwnerLayout />}>
-          <Route
-            path="homepetowner"
-            element={<HomePetowner pet={petcare} product={petproduct} />}
-          />
-          <Route path="petcare" element={<PetCare db={petcare} />} />
-          <Route path="petproduct" element={<PetProduct db={petproduct} />} />
-          <Route path="feedback" element={<FeedBack />} />
-          <Route path="aboutus" element={<AboutUs />} />
-          <Route path="contactus" element={<ContactUs />} />
-          <Route path="emergency" element={<EmerPage />} />
-          <Route path="petcare/:breed" element={<PetDetail db={petcare} />} />
-          <Route
-            path="petproduct/:id"
-            element={<ProductDetail db={petproduct} />}
-          />
+                <Route path="homepetowner" element={<HomePetowner pet={petcare} product={petproduct} />} />
+                <Route path="petcare" element={<PetCare db={petcare} />} />
+                <Route path="petproduct" element={<PetProduct db={petproduct} />} />
+                <Route path="feedback" element={<FeedBack />} />
+                <Route path="aboutus" element={<AboutUs />} />
+                <Route path="contactus" element={<ContactUs />} />
+                <Route path="emergency" element={<EmerPage />} />
+                <Route path="petcare/:breed" element={<PetDetail db={petcare} />} />
+                <Route path="petproduct/:id" element={<ProductDetail db={petproduct} />} />
         </Route>
 
         {/* Veterinarian routes */}
         <Route path="/veterinarian" element={<VetLayout />}>
-          <Route index element={<HomeVet />} />
+          <Route index element={<HomeVet />} /> 
           <Route path=":id" element={<VetProfile />} />
         </Route>
 
@@ -73,25 +70,26 @@ export default function App() {
         <Route path="/animalshelter" element={<AnimalShelterLayout />}>
           <Route path="animal" element={<AnimalShelter />}></Route>
           <Route
-            path="animal/:id"
-            element={<AnimalDetail animal={Animals} />}
-          ></Route>
-          <Route path="story" element={<SuccessStory />}></Route>
-          <Route path="story/:id" element={<StoryDetail />}></Route>
-          <Route path="event" element={<Event />}></Route>
-          <Route path="sheltercontact" element={<ShelterContact />}></Route>
+              path="animal/:id"
+              element={<AnimalDetail animal={Animals} />}
+            ></Route>
+            <Route path="story" element={<SuccessStory />}></Route>
+            <Route
+              path="story/:id"
+              element={<StoryDetail />}
+            ></Route>
+            <Route path="event" element={<Event />}></Route>
+            <Route path="sheltercontact" element={<ShelterContact />}></Route>
         </Route>
 
         {/* 404 */}
-        <Route
-          path="*"
-          element={
-            <div className="container mt-4">
-              <h1>404 - Page Not Found</h1>
-            </div>
-          }
-        />
+        <Route path="*" element={<div className="container mt-4"><h1>404 - Page Not Found</h1></div>} />
       </Routes>
     </Router>
   );
 }
+
+
+
+
+
--- src/pages/Veterinarian/VetProfile.jsx
+++ src/pages/Veterinarian/VetProfile.jsx
@@ -1,31 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import { useParams, Link } from "react-router-dom";
 import vets from "../../Data/Vet/veterinarian.json";
-import BookingModal from "./BookingModal";
 
 const VetProfile = () => {
   const { id } = useParams();
   const vet = vets.find((v) => v.id === parseInt(id));
 
-  // State để hiển thị modal
-  const [showModal, setShowModal] = useState(false);
-
-  const handleBookingNow = () => setShowModal(true);
-  const handleCloseModal = () => setShowModal(false);
-
   if (!vet) {
     return (
       <div className="container mt-4">
         <h2 className="text-danger">Veterinarian not found</h2>
-        <Link
-          to="/veterinarian"
-          className="btn"
-          style={{
-            border: "1px solid black",
-            color: "black",
-            backgroundColor: "transparent",
-          }}
-        >
+        <Link to="/veterinarian" className="btn btn-primary mt-3">
           Back to List
         </Link>
       </div>
@@ -58,46 +43,12 @@ const VetProfile = () => {
             <p>
               <strong>Working Time:</strong> {vet.working_time}
             </p>
-
-            <div className="mt-3 d-flex gap-2">
-              {/* Back to List - nút viền đen */}
-              <Link
-                to="/veterinarian"
-                className="btn"
-                style={{
-                  border: "1px solid black",
-                  color: "black",
-                  backgroundColor: "transparent",
-                }}
-              >
-                ← Back to List
-              </Link>
-
-              {/* Booking Now - nút nền nâu sang trọng */}
-              <button
-                className="btn"
-                style={{
-                  backgroundColor: "#7f5539",
-                  color: "white",
-                  border: "none",
-                }}
-                onClick={handleBookingNow}
-              >
-                Booking Now
-              </button>
-            </div>
+            <Link to="/veterinarian" className="btn btn-outline-primary mt-3">
+              ← Back to List
+            </Link>
           </div>
         </div>
       </div>
-
-      {/* Booking Modal */}
-      {showModal && (
-        <BookingModal
-          show={showModal}
-          vet={vet}
-          handleClose={handleCloseModal}
-        />
-      )}
     </div>
   );
 };
